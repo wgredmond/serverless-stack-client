2751d181f9e02f364dadd6d719ea7450ba7ce79b
William Redmond <bill@wgredmond.com>
2020-02-03 06:06:10 -0500

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "wgr-notes-app-uploads"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://97bb2nb8i3.execute-api.us-east-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_27Raner16",
-      APP_CLIENT_ID: "6avindhccdh62r54i4mm6kuisp",
-      IDENTITY_POOL_ID: "us-east-1:20592a37-cfa3-4c0e-96be-403e638dfc25"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "wgr-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://97bb2nb8i3.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_27Raner16",
+    APP_CLIENT_ID: "6avindhccdh62r54i4mm6kuisp",
+    IDENTITY_POOL_ID: "us-east-1:20592a37-cfa3-4c0e-96be-403e638dfc25"
+  },
+  STRIPE_KEY: "pk_test_qFfa4XIFCmw1ya4GSCopC7BQ00bAc5CSe1"
+};
